b2f884d834e120e825ebfcb10c4ec9c19ac474ef
Foysal Hasan <foysalhasan499@gmail.com>
2022-10-01 04:37:36 -0400

    done

--- js/app.js
+++ js/app.js
@@ -1,20 +1,9 @@
-const slides = document.querySelectorAll('.slide')
-const btnLeft = document.querySelector('.btn-left')
-const btnRight = document.querySelector('.btn-right')
+const panelWrapper = document.querySelector('.container')
+const panels = document.querySelectorAll('.panel')
 
-let currSlide = 0
-const goToSlide = (slide) => slides.forEach((s, i) => (s.style.transform = `translateX(${(i - slide) * 100}%)`))
-goToSlide(0)
-
-const nextSlide = () => {
-  currSlide === slides.length - 1 ? (currSlide = 0) : currSlide++
-  goToSlide(currSlide)
-}
-
-const prevSlide = () => {
-  currSlide === 0 ? (currSlide = slides.length - 1) : currSlide--
-  goToSlide(currSlide)
-}
-
-btnRight.addEventListener('click', nextSlide)
-btnLeft.addEventListener('click', prevSlide)
+panelWrapper.addEventListener('click', function (e) {
+  const currPanel = e.target.closest('.panel')
+  if(!currPanel) return;
+  panels.forEach((panel) => panel.classList.remove('active'))
+  currPanel.classList.add('active')
+})

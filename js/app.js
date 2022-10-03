35b67bd60de094d3fab3dab8c082da750b0419da
Foysal Hasan <foysalhasan499@gmail.com>
2022-10-03 11:48:01 -0400

    done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
-const panelWrapper = document.querySelector('.container')
-const panels = document.querySelectorAll('.panel')
+const revealItems = document.querySelectorAll('.reveal')
 
-panelWrapper.addEventListener('click', function (e) {
-  const currPanel = e.target.closest('.panel')
-  if(!currPanel) return;
-  panels.forEach((panel) => panel.classList.remove('active'))
-  currPanel.classList.add('active')
-})
+const revealElement = () => {
+  const innerHeight = window.innerHeight
+  const revealPoint = 0
+  revealItems.forEach((item) => {
+    const top = item.getBoundingClientRect().top
+    if (top < innerHeight - revealPoint) {
+      item.classList.add('active')
+    } else {
+      item.classList.remove('active')
+    }
+  })
+}
+
+
+window.addEventListener("resize", function() {
+  revealItems.forEach((item) => {
+    item.classList.add('active')
+  })  
+});
+
+window.addEventListener('scroll', revealElement)
